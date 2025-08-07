fe4baffaf8e8aafa2a02b929951fddb437c37079
swap2701 <swapniltomar27@gmail.com>
2025-08-07 14:03:51 +0530

    ETL Pipeline done

---
+++ push_data.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import json
+
+
+from dotenv import load_dotenv  ### used to call .env variables
+load_dotenv()
+
+
+MONGO_DB_URL=os.getenv("MONGO_DB_URL") 
+print(MONGO_DB_URL)
+
+
+import certifi
+ca=certifi.where()
+
+
+import pandas as pd
+import numpy as np
+import pymongo
+from NetworkSecurity.logging.logger import logging
+from NetworkSecurity.exception.Exception import NetworkSecurityException
+
+class NetworkDataExtract(): ### ETL pipeline
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def csv_to_json_convertor(self,file_path): ### First step to convert csv to json
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def insert_data_mongoDB(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
+
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            self.database= self.mongo_client[self.database]
+
+            self.collection=self.database[self.collection]
+
+            self.collection.insert_many(self.records)
+            return(len(self.records))
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+if __name__=="__main__":
+    FILE_PATH="Network_Data\phisingData.csv"
+    DATABASE="SwapnilAI"
+    Collection="NetworkData"
+    networkobj=NetworkDataExtract()
+    records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
+    print(records)
+    no_of_records=networkobj.insert_data_mongoDB(records,DATABASE,Collection)
+    print(no_of_records)
