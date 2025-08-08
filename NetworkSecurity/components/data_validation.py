e43bd3f48237d1e827cbbef32983a233e9cb2779
swap2701 <swapniltomar27@gmail.com>
2025-08-08 19:09:41 +0530

    Data Validation Done

---
+++ NetworkSecurity/components/data_validation.py
@@ -0,0 +1,129 @@
+from NetworkSecurity.entity.config_entity import DataValidationConfig
+from NetworkSecurity.entity.artifact_entity import DataValidationArtifact,DataIngestionArtifact
+
+from NetworkSecurity.logging.logger import logging
+from NetworkSecurity.exception.Exception import NetworkSecurityException
+from NetworkSecurity.constants.training_pipeline import SCHEMA_FILE_PATH
+
+from NetworkSecurity.utils.main_utils.utils import read_yaml_file
+from NetworkSecurity.utils.main_utils.utils import write_yaml_file
+
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+from scipy.stats import ks_2samp
+
+class DataValidation:
+    def __init__(self,data_ingestion_artifacts:DataIngestionArtifact,
+                 data_validation_config:DataValidationConfig):
+        
+        try:
+            self.data_ingestion_artifacts = data_ingestion_artifacts
+            self.data_validation_config = data_validation_config
+            self.schema_config = read_yaml_file(SCHEMA_FILE_PATH)  ### read_yaml_file function in utils folder
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+    ### Read Data Function
+    @staticmethod
+    def read_data(file_path)->pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+    ### Validate the Columns
+    def validate_Number_of_columns(self,dataframe:pd.DataFrame)->bool:
+        try:
+            number_of_columns=len(self.schema_config)
+            logging.info(f"Required number of columns is:{number_of_columns}")
+            logging.info(f"DataFrame has columns: {len(dataframe.columns)}")
+            if len(dataframe.columns)==number_of_columns:
+                return True
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+
+    ### Data_Drift Check
+    def detect_dataset_drift(self,base_df,current_df,threshold=0.05)->bool:
+        try:
+            status=True
+            report={}
+            for column in base_df.columns:
+                d1=base_df[column]
+                d2=current_df[column]
+                is_same_dist=ks_2samp(d1,d2)
+                if threshold<=is_same_dist.pvalue:
+                    is_found=False
+                else:
+                    is_found=True
+                    status=False
+
+                report.update({column:{
+                    "p_value":float(is_same_dist.pvalue),
+                    "drift_status":is_found
+                }})
+
+            drift_report_file_path= self.data_validation_config.drift_report_file_path
+
+            ## Create Directory
+            dir_path =os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            write_yaml_file(file_path=drift_report_file_path,content=report)
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+
+    def initiate_data_validation(self)->DataValidationArtifact:
+        try:
+            train_file_path=self.data_ingestion_artifacts.trained_file_path
+            test_file_path=self.data_ingestion_artifacts.test_file_path
+
+            ### Read the data from Train and Test
+            train_dataframe=DataValidation.read_data(train_file_path)
+            test_dataframe=DataValidation.read_data(test_file_path)
+
+            ### Validate the Number of Columns
+            status=self.validate_Number_of_columns(dataframe=train_dataframe)
+            if not status:
+                error_message=f"Train Dataframe does not contain all columns. \n"
+            status=self.validate_Number_of_columns(dataframe=test_dataframe)
+            if not status:
+                error_message=f"Test Dataframe does not contain all columns. \n"
+
+
+            ### Lets check Data_drift
+            status=self.detect_dataset_drift(base_df=train_dataframe,current_df=test_dataframe)
+            dir_path=os.path.dirname(self.data_validation_config.valid_train_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+
+            train_dataframe.to_csv(
+                self.data_validation_config.valid_train_file_path, index=False, header=True
+            )
+            test_dataframe.to_csv(
+                self.data_validation_config.valid_test_file_path, index=False, header=True
+            )
+
+
+            data_validation_artifact = DataValidationArtifact(
+                validation_status=status,
+                valid_train_file_path=self.data_ingestion_artifacts.trained_file_path,
+                valid_test_file_path=self.data_ingestion_artifacts.test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path
+
+            )
+            return data_validation_artifact
+
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from NetworkSecurity.components.data_ingestion import DataIngestion
+from NetworkSecurity.components.data_validation import DataValidation
 from NetworkSecurity.exception.Exception import NetworkSecurityException
 from NetworkSecurity.logging.logger import logging
-from NetworkSecurity.entity.config_entity import DataIngestionConfig
+from NetworkSecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from NetworkSecurity.entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -13,6 +14,16 @@ if __name__=="__main__":
         data_ingestion=DataIngestion(data_ingestion_config)
         logging.info("Initiate the Data Ingestion")
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(data_ingestion_artifact)
+
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info("Initiate the Data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+
+        
     except Exception as e:
         raise NetworkSecurityException(e,sys)
